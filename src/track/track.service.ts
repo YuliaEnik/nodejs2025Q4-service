d64f037293650c3559e897674f111b5ec4eabb1f
Yuliya Aleinik <aleinikya@mail.ru>
2025-11-28 23:00:19 +0300

    feat: track  module

---
+++ src/track/track.service.ts
@@ -0,0 +1,42 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Track, CreateTrackDto, UpdateTrackDto } from './track.types';
+
+const tracks: Track[] = [];
+
+export const TrackService = {
+
+  findAll(): Track[] {
+    return tracks; 
+  },
+
+  findById(id: string): Track | null {
+    return tracks.find((track) => track.id === id) || null;
+  },
+
+  create(createTrackDto: CreateTrackDto): Track {
+    const newTrack: Track = {
+      id: uuidv4(),
+      ...createTrackDto,
+    };
+    tracks.push(newTrack);
+    return newTrack;
+  },
+
+  update(id: string, updateTrackDto: UpdateTrackDto): Track | null {
+    const trackIndex = tracks.findIndex((track) => track.id === id);
+    if (trackIndex === -1) return null; 
+    const updatedTrack: Track = {
+      ...tracks[trackIndex],
+      ...updateTrackDto, 
+    };
+    tracks[trackIndex] = updatedTrack; 
+    return updatedTrack; 
+  },
+
+  delete(id: string): boolean {
+    const trackIndex = tracks.findIndex((track) => track.id === id);
+    if (trackIndex === -1) return false;
+    tracks.splice(trackIndex, 1); 
+    return true; 
+  },
+};
